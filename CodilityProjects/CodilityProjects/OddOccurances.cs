79474f75b5db07696c622ea2977f38a8112a8560
DESKTOP-Q0OR1CH\HP COMPUTER.S <abdulhameedinfo349@gmail.com>
2023-05-05 12:34:31 +0500

    Find Odd Occurances In Array

---
+++ CodilityProjects/CodilityProjects/OddOccurances.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProjects
+{
+    public static class OddOccurances
+    {
+        public static int solution(int[] A)
+        {
+            // Implement your solution here
+            int result = 0;
+            foreach (int number in A)
+            {
+                result ^= number; // use bitwise XOR operator to find the unpaired element
+            }
+            return result;
+        }
+    }
+}
--- CodilityProjects/CodilityProjects/Program.cs
+++ CodilityProjects/CodilityProjects/Program.cs
@@ -1,10 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CodilityProjects;
 
-var converter = new BinaryConverter();
-var gapFinder = new BinaryGapFinder();
-var printer = new BinaryGapPrinter();
+//var converter = new BinaryConverter();
+//var gapFinder = new BinaryGapFinder();
+//var printer = new BinaryGapPrinter();
 
-var binary = converter.ConvertToBinary(16500);
-var gap = gapFinder.FindMaximumGap(binary);
-printer.PrintMaximumGap(gap);
+//var binary = converter.ConvertToBinary(16500);
+//var gap = gapFinder.FindMaximumGap(binary);
+//printer.PrintMaximumGap(gap);
+
+int[] arr = { -3 , -1 };
+//Console.WriteLine(solution(arr));
+
+int[] arr2 = {42, 1,1, 2, 2,3, 4, 500, 3, 4, 42};
+var oddValue = OddOccurances.solution(arr2) ;
+Console.WriteLine(oddValue);
+int solution(int[] A)
+{
+    // Implement your solution here
+    Array.Sort(A);
+    var list= A.ToList();
+    var smallestElement = 0;
+    var largestElement = A[A.Length - 1] > 0 ? A[A.Length-1] : 0;
+    
+    for(int i= 1; i <= largestElement; i++)
+    {
+        if (list.Exists(x => x == i) == false)
+        {
+            smallestElement = i;
+            break;
+        }
+    }
+
+    return smallestElement > 0 ? smallestElement : largestElement + 1 ;
+}
