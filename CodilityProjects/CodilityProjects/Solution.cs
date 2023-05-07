9dfc1a21667307401f55f39497c3302aa03a2e96
DESKTOP-Q0OR1CH\HP COMPUTER.S <abdulhameedinfo349@gmail.com>
2023-05-07 08:01:32 +0500

    Find the minimum difference between two splits.

---
+++ CodilityProjects/CodilityProjects/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace CodilityProjects
+{
+    public class TapEquilibrium
+    {
+        //List<int> AllSum = new List<int>();
+        public int solution(int[] A)
+        {
+            // Implement your solution here
+
+            var minimumDifference = -1;
+            for (int mid = 1; mid < A.Length; mid++)
+            {
+                int[] firstHalf = A.Take(mid).ToArray();
+                int[] secondHalf = A.Skip(mid).ToArray();
+                var diffrence = Math.Abs(Sum(firstHalf) - Sum(secondHalf));
+                if (diffrence < minimumDifference || minimumDifference == -1) { minimumDifference = diffrence; }
+            }
+            return minimumDifference;
+        }
+        public int Sum(int[] A)
+        {
+            int sum = 0;
+            foreach (var el in A)
+            {
+                sum += el;
+            }
+            return sum;
+        }
+    }
+}
