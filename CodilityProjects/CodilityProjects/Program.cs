db53b05d05c85b06bbb5c4610eaed28bf235782c
DESKTOP-Q0OR1CH\HP COMPUTER.S <abdulhameedinfo349@gmail.com>
2023-06-06 13:49:52 +0500

    A permutation is a sequence containing each element from 1 to N once, and only once. The goal is to check whether array A is a permutation.

--- CodilityProjects/CodilityProjects/Program.cs
+++ CodilityProjects/CodilityProjects/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+Console.WriteLine("Program started");
+Solution solution = new Solution();
+Console.WriteLine(solution.solution(new int[] { 1, 2, 4 }));
+Console.WriteLine(solution.solution(new int[] { 1, 2, 4,3 }));
---
+++ CodilityProjects/CodilityProjects/Solutoin.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public int solution(int[] A)
+    {
+        int distinctCount = A.Distinct().Count();
+        Array.Sort(A);
+
+        return (A[A.Length - 1] == distinctCount && A[A.Length - 1] == A.Length) ? 1 : 0;
+    }
+}
