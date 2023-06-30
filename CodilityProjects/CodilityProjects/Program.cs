2f636cc1b4f5d31e524c40d8f8ea40dfc9e34b72
DESKTOP-Q0OR1CH\HP COMPUTER.S <abdulhameedinfo349@gmail.com>
2023-06-30 13:39:35 +0500

    A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
    Array A contains only 0s and/or 1s:
    
    0 represents a car traveling east,
    1 represents a car traveling west.
    The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
    
    For example, consider array A such that:
    
      A[0] = 0
      A[1] = 1
      A[2] = 0
      A[3] = 1
      A[4] = 1
    We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
    
    Write a function:
    
    class Solution { public int solution(int[] A); }
    
    that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
    
    The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.

--- CodilityProjects/CodilityProjects/Program.cs
+++ CodilityProjects/CodilityProjects/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CodilityProjects;
+
 Console.WriteLine("Hello, World!");
+
+Solution solution = new Solution();
+int passingPairs = solution.NoOfPassingCars(new int[] { 1, 0, 0, 0, 1, 0, 1});
+
+Console.WriteLine("No of passing pairs: " + passingPairs);
---
+++ CodilityProjects/CodilityProjects/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProjects
+{
+    public class Solution
+    {
+        public int NoOfPassingCars(int[] A)
+        {
+            int ZerrosCount = 0;
+            int PassingPairs = 0;
+            foreach (int car in A)
+            {
+                if (car == 0)
+                    ZerrosCount++;
+                else
+                {
+                    PassingPairs += ZerrosCount;
+                    if (PassingPairs > 100000)
+                        return -1;
+                }
+            }
+            return PassingPairs;
+        }
+    }
+}
