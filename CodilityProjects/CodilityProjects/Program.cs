ac8d054acf66f37ba2afcbea7482da59e1a7728f
DESKTOP-Q0OR1CH\HP COMPUTER.S <abdulhameedinfo349@gmail.com>
2023-07-03 13:26:51 +0500

    Write a function:
    class Solution { public int solution(int A, int B, int K); }
    
    that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
    
    { i : A ≤ i ≤ B, i mod K = 0 }
    
    For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.

--- CodilityProjects/CodilityProjects/Program.cs
+++ CodilityProjects/CodilityProjects/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Solution solution = new Solution();
+int A = 7;
+int B = 12;
+int K = 3;
+int divisibleByK = solution.solution(A, B, K);
+Console.WriteLine(String.Format("Number of integers divisible by {0} between {1} & {2} is = {3}", K, A, B,
+    divisibleByK));
---
+++ CodilityProjects/CodilityProjects/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+class Solution
+{
+    public int solution(int A, int B, int K)
+    {
+        // Implement your solution here
+        int divisibleByK = 0;
+
+        if (A % K == 0)
+            divisibleByK = (B - A) / K + 1;
+        else
+            divisibleByK = (B - (A - A % K)) / K;
+
+        return divisibleByK;
+    }
+}
