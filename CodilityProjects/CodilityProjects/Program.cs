0739b3f16e18ee485e730f59a5d77a713bc0d601
DESKTOP-Q0OR1CH\HP COMPUTER.S <abdulhameedinfo349@gmail.com>
2023-07-08 22:18:54 +0500

    QuickSort is a sorting algorith based on divide and conquer algorithm, that picks an element as a pivot and partition the array arroud the picked by placing the pivot in itc correct position.

--- CodilityProjects/CodilityProjects/Program.cs
+++ CodilityProjects/CodilityProjects/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodilityProjects;
+
+int[] arrayToSort = { 4, 2, 3, 1, 8, 6, 9, 5, 7, 10 };
+Solution.Sort(arrayToSort);
---
+++ CodilityProjects/CodilityProjects/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityProjects
+{
+    internal class Solution
+    {
+        public static void Swap(int[] arr, int a, int b)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+        public static int Partition(int[] arr, int low, int high)
+        {
+            int i = low - 1; //lowest element to compare
+            int pivot = arr[high];
+
+            for(int j = low; j < high; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+            Swap(arr, i + 1, high);
+            return i + 1;
+        }
+        public static void QuickSort(int[] arr, int low, int high)
+        {
+            if(low < high)
+            {
+                int pi = Partition(arr, low, high);
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
+            }
+        }
+        public static void Sort(int[] arr)
+        {
+            Console.WriteLine("Un-Sorted array: " + string.Join(", ", arr));
+            QuickSort(arr, 0, arr.Length - 1);
+            Console.WriteLine("Sorted array after QUICK SORT: \n" + string.Join(", ", arr));
+        }
+    }
+}
